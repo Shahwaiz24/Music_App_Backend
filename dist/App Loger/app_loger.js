d0d0ec7100d730fa7fd59b310561a46ed349f63f
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-08-19 12:08:11 -0700

    Updated code of Request Track

--- dist/App Loger/app_loger.js
+++ dist/App Loger/app_loger.js
@@ -1,11 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-let AppLoger = (request, response, next) => {
+const database_1 = __importDefault(require("../config/database"));
+let AppLoger = async (request, response, next) => {
     let date = new Date().toLocaleDateString();
     let time = new Date().toLocaleTimeString();
     let Method = request.method;
     let url = request.url;
     console.log(`${date} | ${time} | ${Method} | ${url}`);
+    let db = await database_1.default.getDatabase();
+    let requestCollection = db.collection('requests_tracks');
+    await requestCollection.insertOne({
+        'Date': date,
+        'Time': time,
+        'Method': Method.toString(),
+        'url': url.toString()
+    });
     next();
 };
 exports.default = AppLoger;
--- src/App loger/app_loger.ts
+++ src/App loger/app_loger.ts
@@ -1,13 +1,28 @@
 
 import express from "express";
+import Database from "../config/database";
+import { Db } from "mongodb";
 
-let AppLoger = (request: express.Request, response: express.Response, next: express.NextFunction) => {
+
+let AppLoger = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
     let date = new Date().toLocaleDateString();
     let time = new Date().toLocaleTimeString();
     let Method = request.method;
     let url = request.url;
 
     console.log(`${date} | ${time} | ${Method} | ${url}`);
+
+
+
+    let db: Db = await Database.getDatabase();
+    let requestCollection = db.collection('requests_tracks');
+    await requestCollection.insertOne({
+        'Date': date,
+        'Time': time,
+        'Method': Method.toString(),
+        'url': url.toString()
+    });
+
     next();
 }
 
